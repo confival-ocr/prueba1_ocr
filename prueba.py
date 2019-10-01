56991b013484f26d24046601f4f35beff763a2a3
jjmuesesq <jjmuesesq@unal.edu.co>
2019-10-01 17:52:53 -0500

    primer version script ocr

---
+++ prueba.py
@@ -0,0 +1,47 @@
+#====================================================================================
+#--> IMPORTAMOS LA LIBRERIA PARA OCR EN PYTHON 3 PYTESERACT 
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+#--> UBICACION PAQUETE TESSERACT EN LOCAL
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+
+print(" ===================================")
+print("|      PROBANDO OCR CONFIVAL        |")
+print(" ===================================")
+
+#====================================================================================
+print("=============================================================")
+print("SCRIPT EN PYTHON PARA RECONOCIMIENTO OPTICO DE CARACTERES OCR")
+print("=============================================================")
+
+#====================================================================================
+#--->SE GUARDA LOS DATOS DE IMAGEN PROCESADA POR PYTESSERACT 
+d = pytesseract.image_to_string(Image.open("jhon5.png"))
+#print (d) imprime el string de datos obtenidos a traves de pytesseract
+#print(type(d)) retorna el tipo de dato para d
+
+#-->SE DIVIDE EL STRING DE DATOS EN ITEMS PARA ALMACENAR EN UNA LISTA
+datos = d.split() 
+#print(type(datos)) retorna el tipo de dato para la variable datos ()
+print(len(datos))
+
+cedula = (datos[9:10])
+print(type(cedula))
+
+def clasificacion():
+    return 1
+
+
+
+#datos_cedula=[79912457,"alejandro","juyo"]
+#var_cedula={"numero":datos_cedula[0], "nombre":datos_cedula[1], "apellido":datos_cedula[2]}
+#print(var_cedula)
+
+#manipulación de cadenas en Python
+#como cortar cadenas en python
+#como pegar cadenas en python 
+ 
