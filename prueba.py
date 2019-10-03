0bb3c7e373e4f1fe6a11cbac6c9ae18927527803
jjmuesesq <jjmuesesq@unal.edu.co>
2019-10-03 11:46:41 -0500

    clasificando datos cedula lado 1

--- prueba.py
+++ prueba.py
@@ -20,30 +20,46 @@ print("=============================================================")
 
 #====================================================================================
 #--->SE GUARDA LOS DATOS DE IMAGEN PROCESADA POR PYTESSERACT 
-d = pytesseract.image_to_string(Image.open("jhon5.png"))
-#print (d) imprime el string de datos obtenidos a traves de pytesseract
-#print(type(d)) retorna el tipo de dato para d
+d = pytesseract.image_to_string(Image.open("resolucionAceptable1.png"))
+#print(d) #imprime el string de datos obtenidos a traves de pytesseract
 
 #-->SE DIVIDE EL STRING DE DATOS EN ITEMS PARA ALMACENAR EN UNA LISTA
-datos = d.split() 
+datos = d.splitlines() 
 #print(type(datos)) retorna el tipo de dato para la variable datos ()
 print(datos)
+datos_cedula = []
+temp1 = datos[3]
+tempcedula = temp1.split(" ")
+#print(tempcedula)
+cedula = tempcedula[1].replace(".", "")
+#print(cedula) 
+NumCedula = int(cedula)
+print("==================================================")
+print("Numero de Cedula: ", NumCedula)
+print(type(NumCedula))
+datos_cedula.append(NumCedula)
+print("==================================================")
 
-cedula = (datos[9:10])
-print("Identificacion  = ", cedula)
+print("==================================================")
+nombres = datos[6]
+print("Nombres: ", nombres)
+print(type(nombres))
+datos_cedula.append(nombres)
+print("==================================================")
 
-nombres = (datos[12:14])
-print("Nombres = ", nombres)
+print("==================================================")
+apellidos = datos[4]
+print("Apellidos: ", apellidos)
+print(type(apellidos))
+datos_cedula.append(apellidos)
+print("==================================================")
 
-apellidos = (datos[10:12])
-print("Apellidos = ", apellidos)
-#print(cedula.replace('.', ''))
+print("==================================================")
+print("arreglo de datos cedula")
+print("Datos cedula => ", datos_cedula)
 
-
-datos_cedula=[79912457,"alejandro","juyo"]
+print("==================================================")
+print("diccionario variable cedula")
 var_cedula={"numero":datos_cedula[0], "nombre":datos_cedula[1], "apellido":datos_cedula[2]}
-print(var_cedula)
+print("var_cedula => ", var_cedula)
 
-#manipulación de cadenas en Python
-#como cortar cadenas en python
-#como pegar cadenas en python 
